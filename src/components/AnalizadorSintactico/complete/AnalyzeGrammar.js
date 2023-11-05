3555297c1bef5cfc3d17e5f299dd80bc7dd8d35c
Corne <cornejoedwin053@gmail.com>
2023-11-04 23:46:20 -0600

    rules and styles added

--- src/components/AnalizadorSintactico/complete/AnalyzeGrammar.js
+++ src/components/AnalizadorSintactico/complete/AnalyzeGrammar.js
@@ -27,53 +27,58 @@ export const analyze = (input) => {
   const history = []; // to save all the process
   // expantion and reduccion of the input
   let token = 0;
-  while (token < tokens.length) {
-    //crste a new array from token to the end of the array and extract only lexema value
-    const lexemas = tokens.slice(token).map((token) => token.lexema);
-    const subString = lexemas.join(" ");
+  try {
+    while (token < tokens.length) {
+      //crste a new array from token to the end of the array and extract only lexema value
+      const lexemas = tokens.slice(token).map((token) => token.lexema);
+      const subString = lexemas.join(" ");
 
-    const stackTop = stack[stack.length - 1].valorEP; // the row of the LR1 Table
-    const inputTop = tokens[token].shortened; // the column of the LR1 Table
-    const accion = lrTable[stackTop][inputTop]; // the accion that was retuted by lr1Table
+      const stackTop = stack[stack.length - 1].valorEP; // the row of the LR1 Table
+      const inputTop = tokens[token].shortened; // the column of the LR1 Table
+      console.log(stackTop, "--", inputTop);
+      const accion = lrTable[stackTop][inputTop]; // the accion that was retuted by lr1Table
 
-    const stackCopy = [...stack]; // create a copy of the actual stack
-    // save the data of actual satck, input and the output
-    history.push({
-      stack: stackCopy,
-      input: subString,
-      output: accion,
-    });
+      const stackCopy = [...stack]; // create a copy of the actual stack
+      // save the data of actual satck, input and the output
+      history.push({
+        stack: stackCopy,
+        input: subString,
+        output: accion,
+      });
 
-    // validate if the accion is a reduction or not
-    if (accion < 0) {
-      //reduction
-      //get the corresponding rule
-      const rule = rules[Math.abs(accion)];
+      // validate if the accion is a reduction or not
+      if (accion < 0) {
+        //reduction
+        //get the corresponding rule
+        const rule = rules[Math.abs(accion)];
 
-      // remove elements from the stack
-      for (let i = 0; i < rule.symbolsNumber * 2; i++) {
-        stack.pop();
+        // remove elements from the stack
+        for (let i = 0; i < rule.symbolsNumber * 2; i++) {
+          stack.pop();
+        }
+
+        const stackTop = stack[stack.length - 1].valorEP; //new stackTop after remove elements from the stack
+
+        const reductionAccion = lrTable[stackTop][rule.noTerminal]; // the accion that was retuted by lr1Table
+
+        const newNoTerminal = new NoTerminal(rule.noTerminal); // add the no terminal of the rule
+        stack.push(newNoTerminal);
+
+        const newElemento = new Elemento(reductionAccion); // add the element to the stack
+        stack.push(newElemento);
+        console.log(stack);
+      } else {
+        //expantion
+        // create new objects for the input and output and add them to the stack
+        const newTerminal = new Terminal(tokens[token].lexema);
+        stack.push(newTerminal);
+        const newElemento = new Elemento(accion);
+        stack.push(newElemento);
+        token++; // only move to the next token if is an expantion
       }
-
-      const stackTop = stack[stack.length - 1].valorEP; //new stackTop after remove elements from the stack
-
-      const reductionAccion = lrTable[stackTop][rule.noTerminal]; // the accion that was retuted by lr1Table
-
-      const newNoTerminal = new NoTerminal(rule.noTerminal); // add the no terminal of the rule
-      stack.push(newNoTerminal);
-
-      const newElemento = new Elemento(reductionAccion); // add the element to the stack
-      stack.push(newElemento);
-      //console.log(stack)
-    } else {
-      //expantion
-      // create new objects for the input and output and add them to the stack
-      const newTerminal = new Terminal(tokens[token].lexema);
-      stack.push(newTerminal);
-      const newElemento = new Elemento(accion);
-      stack.push(newElemento);
-      token++; // only move to the next token if is an expantion
     }
+  } catch (e) {
+    console.error(e);
   }
   console.log(stack);
   console.log("history final", history);
--- src/components/AnalizadorSintactico/complete/SintacticPage.jsx
+++ src/components/AnalizadorSintactico/complete/SintacticPage.jsx
@@ -1,65 +1,66 @@
 import React, { useState } from "react";
 import { analyze } from "./AnalyzeGrammar";
-import "../../../styles/lexicalAn.css";
+import "../../../styles/sintactic.css";
 
 const SintacticPage = () => {
   const [input, setInput] = useState(""); // Entrada del usuario
   const [sintacticData, setSintacticData] = useState([]);
+  const [showTable, setShowTable] = useState(false);
+  const [transitionTable, setTransitionTable] = useState("in:wipe:down");
 
   const handleSintacticAnalyze = () => {
     setSintacticData(analyze(input));
+    setTransitionTable("in:wipe:down");
+    setShowTable(true);
   };
 
   const clean = () => {
     setInput("");
+    setTransitionTable("out:wipe:up");
   };
 
   return (
-    <div className="page">
-      <h2 className="tokensArea__title">Sintactic analyzer </h2>
-      <div className="complete__page">
-        <div className="inputArea">
-          <h2 className="tokensArea__title">Inputs</h2>
-          <textarea
-            placeholder="Ingrese la entrada aquí"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            className="inputArea__area"
-          />
-          <button
-            onClick={handleSintacticAnalyze}
-            className="inputArea__button"
-          >
-            Analyze
-          </button>
-          <button onClick={clean} className="inputArea__button --clean">
+    <div className="mainContainer" transition-style={"in:wipe:down"}>
+      <h2 className="mainContainer__title">Sintactic analyzer </h2>
+
+      <div className="codeContainer">
+        <h2 className="subTitle">Inputs</h2>
+        <textarea
+          placeholder="Ingrese la entrada aquí"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="codeContainer__area shadow-drop-center"
+        />
+        <div className="btnsContaniner">
+          <button onClick={clean} className="--clean scale-down-center">
             Clean
           </button>
+          <button onClick={handleSintacticAnalyze} className=" --analyze scale-down-center ">
+            Analyze
+          </button>
         </div>
-        <div className="tokensArea">
-          <h2 className="tokensArea__title">Syntactic analysis</h2>
-          <table className="tokenArea__table">
-            <thead className="tokenArea__row">
-              <tr>
-                <th className="tokenArea__column">Stack</th>
-                <th className="tokenArea__column">Input</th>
-                <th className="tokenArea__column">Output</th>
-              </tr>
-            </thead>
+      </div>
+      {showTable && (
+        <div  className="processContainer table-container" transition-style={transitionTable}>
+          <h2 className="subTitle">Syntactic analysis</h2>
+          <table className="processContainer__table">
+            <tr className="tableHead">
+              <th className="tableHead__column --radiusLeft">Stack</th>
+              <th className="tableHead__column">Input</th>
+              <th className="tableHead__column --radiusRight">Output</th>
+            </tr>
             {sintacticData.map((process, index) => (
-              <tbody key={index} className="table__row">
-                <tr>
-                  <td className="table__column">
-                    {process.stack.map((item) => item.valorEP)}
-                  </td>
-                  <td className="table__column">{process.input}</td>
-                  <td className="table__column">{process.output}</td>
-                </tr>
-              </tbody>
+              <tr key={index} className="tableBody">
+                <td className="tableBody__column">
+                  {process.stack.map((item) => item.valorEP)}
+                </td>
+                <td className="tableBody__column">{process.input}</td>
+                <td className="tableBody__column">{process.output}</td>
+              </tr>
             ))}
           </table>
         </div>
-      </div>
+      )}
     </div>
   );
 };
